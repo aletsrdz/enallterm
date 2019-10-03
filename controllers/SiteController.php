de0c2abaf485b8f8c3032e1d130acb176e5ed5e8
Alets Rdz <alejandro@enallt.unam.mx>
2019-10-03 12:47:45 -0500

    Modificación de los archivos Sitecontroller para que funcione guarda datos de la pagina de registro. config/web descomente el prettyUrl. models/User agregue a la base de datos la tabla "user" e hice modificaciones a las funciones beforeSave y beforeValidate para que guarde en la tabla user site/login no me funciona site/register modificacion de este archivo para que se vea como formulario

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\User;
 
 class SiteController extends Controller
 {
@@ -80,9 +81,22 @@ class SiteController extends Controller
             return $this->goBack();
         }
 
-        $model->password = '';
         return $this->render('login', [
             'model' => $model,
+        ]);
+    }
+
+    public function actionRegister(){
+        if(!Yii::$app->user->isGuest){
+            return $this->goHome();
+        }
+        $newUser = new User();
+        if ($newUser->load(Yii::$app->request->post()) && $newUser->save()) {
+            Yii::$app->session->setFlash('success', Yii::t('app', 'Registro exitoso'));
+            return $this->goHome();
+        }
+        return $this->render('register', [
+            'newUser'=> $newUser
         ]);
     }
 
--- models/User.php
+++ models/User.php
@@ -2,103 +2,91 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+use yii\db\Expression;
+
+
+/**
+ * This is the model class for table "{{%user}}".
+ *
+ * @property int $id
+ * @property string $uid
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property int $contact_email
+ * @property int $contact_phone
+ * @property bool $status
+ * @property string $created_at
+ * @property string $update_at
+ */
+class User extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
+    const STATUS_INSERTED = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_BLOQUED = 2;
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentity($id)
+    public static function tableName()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return '{{%user}}';
     }
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function rules()
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return [
+            [['uid','username', 'email', 'password'], 'required'],
+            [['status','contact_email', 'contact_phone'], 'integer'],
+            [['email'], 'email'],
+            [['created_at', 'update_at'], 'safe'],
+            [['uid', 'password'], 'string', 'max'=>60],
+            [['username'], 'string', 'max'=>45],
+            [['email'], 'string', 'max'=>255],
+            [['uid'], 'unique'],
+            [['email'], 'unique'],
+        ];
     }
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+    public function beforeValidate() {
+        if($this->isNewRecord){
+            $this->setUid();    
+        }        
+        return parent::beforeValidate();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
+    public function beforeSave($insert) {
+        if($this->isNewRecord){
+            $this->password= Yii::$app->getSecurity()->generatePasswordHash($this->password);    
+        }  
+        $this->update_at =  new Expression('NOW()');
+        return parent::beforeValidate();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
+    private function setUid(){
+        $this->uid = Yii::$app->getSecurity()->generatePasswordHash(date('YmdHis').rand(1,999999));
     }
 
     /**
      * {@inheritdoc}
      */
-    public function validateAuthKey($authKey)
+    public function attributeLabels()
     {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
+        return [
+            'id' => 'ID',
+            'uid' => 'Uid',
+            'username' => 'Username',
+            'email' => 'Email',
+            'password' => 'Password',
+            'contact_email' => 'Contact Email',
+            'contact_phone' => 'Contact Phone',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'update_at' => 'Update At',
+        ];
     }
 }
